5a964c81bf53efa4384aab6d10a4c81715b38652
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-03 13:02:35 +0545

    created a frame and change basic format of application like title, logo and bg colour

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main() {
-        System.out.println("hello world");
+        JFrame frame = new JFrame();// create a frame
+
+        frame.setSize(420,420); // sets x dimension and y dimension
+        frame.setTitle("One Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+        frame.setResizable(false);// prevent frame being resize
+        // change the logo
+        // ImageIcon image = new ImageIcon("file path");
+//        frame.setIconImage(image.getImage());
+
+        frame.setVisible(true);// make frame visible
+
+        frame.getContentPane().setBackground(Color.BLACK); // background colour
     }
 }
